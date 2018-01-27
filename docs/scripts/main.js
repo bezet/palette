e41d32b683cbe034a21bb877d1b26074e8a721c2
bezet <bartosz.zadara@gmail.com>
2018-01-27 13:01:47 +0100

    Use new parameter

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,2 +1,8 @@
 import Palette from '@bezet/palette';
-const myPalette = new Palette();
+const myPalette = new Palette({ colors: [
+  '#326273',
+  '#E39774',
+  'rgba(92, 158, 173, 1)',
+  'rgba(238, 238, 238, 1)',
+  'rgba(255, 255, 255, 1)',
+] });
