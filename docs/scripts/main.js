7257ade2b98b4c120e4363c9b3d4be71bc08e4a8
bezet <bartosz.zadara@gmail.com>
2018-01-28 12:01:08 +0100

    Add body class toggle for demo's purposes

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,8 +1,16 @@
 import Palette from '@bezet/palette';
+
 const myPalette = new Palette({ colors: [
-  '#326273',
-  '#E39774',
-  'rgba(92, 158, 173, 1)',
-  'rgba(238, 238, 238, 1)',
-  'rgba(255, 255, 255, 1)',
+  'rgba(117, 79, 68, 1)',
+  'rgba(236, 115, 87, 1)',
+  'rgba(225, 206, 122, 1)',
+  'rgba(253, 214, 146, 1)',
+  'rgba(251, 255, 185, 1)',
 ] });
+
+const paletteButton = document.querySelector('.color-palette__btn');
+
+paletteButton.addEventListener('click', event => {
+  const theBody = document.querySelector('body');
+  theBody.classList.toggle('js-active');
+});
